c18409e12d1d06eeba44479f4209157e7004a543
Iw <iwor.java@gmail.com>
2023-06-12 13:11:15 +0400

    4. @Before & @After

--- src/test/java/com/iwor/junit/service/UserServiceTest.java
+++ src/test/java/com/iwor/junit/service/UserServiceTest.java
@@ -1,27 +1,57 @@
 package com.iwor.junit.service;
 
 import com.iwor.junit.dto.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class UserServiceTest {
+    private UserService userService;
+
+    @BeforeAll
+    static void init() {
+        System.out.println("\nBefore all");
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("\nBefore each: " + this);
+        userService = new UserService();
+    }
 
     @Test
     void usersEmptyIfNoUserAdded() {
-        var userService = new UserService();
-        var users = userService.getAll();
+        System.out.println("Test 1: " + this);
+        List<User> users = userService.getAll();
         assertTrue(users.isEmpty(), () -> "List of users should be empty");
     }
 
     @Test
     void usersSizeIfUserAdded() {
-        var userService = new UserService();
+        System.out.println("Test 2: " + this);
         userService.add(new User());
         userService.add(new User());
 
-        var users = userService.getAll();
+        List<User> users = userService.getAll();
         assertEquals(2, userService.getAll().size());
+    }
+
+    @AfterEach
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
+    }
+
+    @AfterAll
+    static void closeConnectionPool() {
+        System.out.println("\nAfter all");
     }
 }
